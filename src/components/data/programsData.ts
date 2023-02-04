696c845a9d8cca2c61bac9a3bfd932aec249e79f
Alternatio <shinkarev0913@mail.ru>
2023-02-04 21:44:33 +0600

    Create programsData.ts

---
+++ src/components/data/programsData.ts
@@ -0,0 +1,24 @@
+interface programI {
+  title: string
+  description: string
+  steps: string[]
+}
+
+export const data: programI[] = [
+  {
+    title: '',
+    description: '',
+    steps: [
+      '',
+      ''
+    ]
+  },
+  {
+    title: '',
+    description: '',
+    steps: [
+      '',
+      ''
+    ]
+  }
+]
