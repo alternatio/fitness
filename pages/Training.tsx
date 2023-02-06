1d299e0601a14ed3747dc880f8ed633792b6d78e
Alternatio <shinkarev0913@mail.ru>
2023-02-06 22:03:04 +0600

    small changes

---
+++ pages/Training.tsx
@@ -0,0 +1,29 @@
+import { NextPage } from 'next'
+import { memo } from 'react'
+import Header from '../src/components/Header/Header'
+import GlobalWrapper from '../src/ui/GlobalWrapper/GlobalWrapper'
+import style from '/styles/pages/Training.module.scss'
+import {data} from "../src/components/data/programsData";
+import { motion } from 'framer-motion'
+
+const Training: NextPage = () => {
+	return (
+		<GlobalWrapper>
+			<Header />
+			<div className={style.listWrapper}>
+				<motion.div layout={'size'} className={style.trainingsList}>
+					{data.map((value, index) => {
+						return (
+							<motion.div layout={'size'}
+								className={style.block}>
+								{value.title}
+							</motion.div>
+						)
+					})}
+				</motion.div>
+			</div>
+		</GlobalWrapper>
+	)
+}
+
+export default memo(Training)
--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,30 +1,34 @@
 import { NextPage } from 'next'
 import { memo } from 'react'
 import style from './styles/Intro.module.scss'
-import Image from "next/image"
+import Image from 'next/image'
 import sculpture from '/src/components/images/sculpture.png'
 
-interface IntroProps {
-
-}
+interface IntroProps {}
 
 const Intro: NextPage<IntroProps> = props => {
-  return (
-    <div className={style.intro}>
-      <div className={style.cube}>
-        <Image className={style.image} src={sculpture} alt={'sculpture'} />
-      </div>
-      <h3 className={style.title}>
-        <span>Мы создаём для вас силу.</span>
-        <span>We create power for you.</span>
-        <span>Nous créons la force pour vous.</span>
-        <span>Nos enim potentia creare.</span>
-      </h3>
-      <p className={style.description}>
-        Создаём вместе программы тренировок и питания, чтобы быть сильнее. С помощью наших программ вы точно сможете покорить Олимп!
-      </p>
-    </div>
-  )
+	return (
+		<div className={style.intro}>
+			<div className={style.cube}>
+				<Image
+					className={style.image}
+					src={sculpture}
+					alt={'sculpture'}
+				/>
+			</div>
+			<h3 className={style.title}>
+				<span>Мы создаём для вас силу.</span>
+				<span>We create power for you.</span>
+				<span>Nous créons la force pour vous.</span>
+				<span>Nos enim potentia creare.</span>
+			</h3>
+			<p className={style.description}>
+				Создаём вместе программы тренировок и питания, чтобы
+				быть сильнее. С помощью наших программ вы точно
+				сможете покорить Олимп!
+			</p>
+		</div>
+	)
 }
 
 Intro.displayName = 'Intro'
