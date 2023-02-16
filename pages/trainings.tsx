8fae31fe892397da174583dfab247c2cce908c6c
Alternatio <shinkarev0913@mail.ru>
2023-02-16 12:45:42 +0600

    release

--- pages/trainings.tsx
+++ pages/trainings.tsx
@@ -14,6 +14,7 @@ import InputText from '../src/ui/InputText/InputText'
 import InputRadio from '../src/ui/InputRadio/InputRadio'
 import Head from 'next/head'
 import Footer from '../src/components/Footer/Footer'
+import { motion } from 'framer-motion'
 
 interface searchI {
 	stage: stageT
@@ -121,7 +122,7 @@ const Trainings: NextPage = () => {
 				</div>
 
 				<div className={style.trainingListWrapper}>
-					<div className={style.trainingsList}>
+					<motion.div layout={true} className={style.trainingsList}>
 						{!search &&
 							data.map((value, index) => {
 								return (
@@ -152,7 +153,7 @@ const Trainings: NextPage = () => {
 										/>
 									)
 								})}
-					</div>
+					</motion.div>
 				</div>
 
 				<Footer />
--- src/components/TrainingBlock/TrainingBlock.tsx
+++ src/components/TrainingBlock/TrainingBlock.tsx
@@ -3,6 +3,8 @@ import style from './styles/TrainingBlock.module.scss'
 import {data, programI} from '../data/programsData'
 import Image from "next/image";
 import Link from "next/link";
+import {AnimatePresence, motion, Variants} from 'framer-motion';
+import {commonAnimations, commonTransition} from "../../ui/helpers/animations";
 
 interface TrainingBlockProps {
 	object: programI
@@ -14,25 +16,44 @@ const TrainingBlock: FC<TrainingBlockProps> = props => {
   const image = require(`/public/images/${props.object.image}`)
   const currentIndex = data.indexOf(props.object)
 
+  const variantsBlock: Variants = {
+    off: {
+      opacity: 0
+    },
+    on: {
+      opacity: 1
+    }
+  }
+
 	return (
-    <Link href={`/training/${currentIndex}`} className={style.block}>
-      <Image className={style.image} src={image} alt={'чел'} />
-      <h3 className={style.title}>{props.object.title}</h3>
-      <div className={style.description}>
+    <AnimatePresence>
+      <motion.div
+        variants={variantsBlock}
+        {...commonAnimations}
+        transition={commonTransition(.25)}
+        layout={true}
+        layoutId={currentIndex+''}
+        className={style.blockWrapper}>
+        <Link href={`/training/${currentIndex}`} className={style.block}>
+          <Image className={style.image} src={image} alt={'чел'} />
+          <h3 className={style.title}>{props.object.title}</h3>
+          <div className={style.description}>
         <span className={style.descriptionText}>
           {props.object.description}
         </span>
-        <span className={style.descriptionStage}>
+            <span className={style.descriptionStage}>
           {props.arrayOfTextStage[props.object.stage]}
         </span>
-        <span className={style.descriptionPurpose}>
+            <span className={style.descriptionPurpose}>
           Тип тренировки: {props.object.purpose}
         </span>
-        <span className={style.descriptionStepsLength}>
+            <span className={style.descriptionStepsLength}>
           Количество упражнений: {props.object.steps.length}
         </span>
-      </div>
-    </Link>
+          </div>
+        </Link>
+      </motion.div>
+    </AnimatePresence>
   )
 }
 
